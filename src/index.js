48d3e16ef92762d8a8837bb6bdf02b78940964f5
Edgar <edgar.nurullin@onetwotrip.com>
2019-02-26 15:25:09 +0300

    Revert "add tests infrastructure and simple test"
    This reverts commit 9870ea3922f17711a7c5f56657f7097a14b48201.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,118 @@
-import patch from './patch';
+class Logger {
+  constructor(componentName) {
+    this._componentName = componentName;
+    this._propsChanges = [];
+    this._stateChanges = [];
+    this._renderCount = 0;
+  }
 
-export default (...args) => {
+  init() {
+    console.log('%c[clear-render] init for', 'color: #848d95;', this._componentName);
+    console.log('%c[clear-render] render', 'color: #848d95;', this._componentName);
+  }
+
+  _printChange(change) {
+    if (typeof change === 'object') {
+      console.groupCollapsed(`${change.key} %c [${change.type}]`, 'color: #848d95; font-style: italic;');
+      console.log('%c old ', 'background: #ff6347; color: #fff', change.oldValue);
+      console.log('%c new ', 'background: #5fba7d; color: #fff', change.nextValue);
+      console.groupEnd();
+    } else {
+      console.log(change);
+    }
+  }
+
+  printComparisonsResults() {
+    console.group(`%c[clear-render] re-render #${this._renderCount}`, 'color: #848d95;', this._componentName);
+    this._printComparisonResult('props', this._propsChanges);
+    this._printComparisonResult('state', this._stateChanges);
+    console.groupEnd();
+  }
+
+  _printComparisonResult(title, changes = []) {
+    if (changes.length === 0) {
+      return;
+    }
+    console.group(title);
+    changes.forEach(this._printChange);
+    console.groupEnd();
+  }
+
+  _differenceBetweenObjects(oldObj = {}, nextObj = {}) {
+    if (!nextObj || !oldObj) {
+      return [];
+    }
+    let difference = [];
+    Object.keys(nextObj).forEach(key => {
+      if (nextObj[key] !== oldObj[key]) {
+        const type = typeof nextObj[key];
+        difference.push({
+          key,
+          type,
+          oldValue: oldObj[key],
+          nextValue: nextObj[key],
+        });
+      }
+    });
+    return difference;
+  }
+
+  shallowCompareProps(prevProps, nextProps) {
+    this._propsChanges = this._differenceBetweenObjects(prevProps, nextProps);
+  }
+
+  shallowCompareState(prevState, nextState) {
+    this._stateChanges = this._differenceBetweenObjects(prevState, nextState);
+  }
+
+  get isFirstRender() {
+    return !this._renderCount;
+  }
+
+  incrementRenderCount() {
+    this._renderCount++;
+  }
+};
+
+const wrap = (ComponentClass, forcedDisplayedName) => {
+  const componentClassName = ComponentClass.name;
+
+  const logger = new Logger(forcedDisplayedName || componentClassName);
+
+  const originalRender = ComponentClass.prototype.render;
+  ComponentClass.prototype.render = function () {
+    if (logger.isFirstRender) {
+      logger.init();
+    }
+
+    logger.incrementRenderCount();
+
+    if (originalRender) {
+      return originalRender.call(this);
+    } else {
+      return null;
+    }
+  }
+
+  const originalСomponentDidUpdate = ComponentClass.prototype.componentDidUpdate;
+  ComponentClass.prototype.componentDidUpdate = function (...args) {
+    const [prevProps, prevState] = args;
+    const nextProps = this.props;
+    const nextState = this.state;
+
+    logger.shallowCompareState(prevState, nextState);
+    logger.shallowCompareProps(prevProps, nextProps);
+    logger.printComparisonsResults();
+
+    if (originalСomponentDidUpdate) {
+      return originalСomponentDidUpdate.call(this, ...args);
+    }
+  }
+
+  return ComponentClass;
+};
+
+module.exports = function (...args) {
   if (args.length > 2) {
     console.error('[clear-render] Error: Too many arguments');
     return null;
@@ -19,15 +131,13 @@ export default (...args) => {
 
     switch (type) {
       case 'symbol':
-      case 'string':
-        {
-          forcedDisplayedName = arg;
-        }
+      case 'string': {
+        forcedDisplayedName = arg;
+      }
         break;
-      default:
-        {
-          ComponentClass = arg;
-        }
+      default: {
+        ComponentClass = arg;
+      }
         break;
     }
   });
@@ -37,7 +147,5 @@ export default (...args) => {
     return null;
   }
 
-  const name = forcedDisplayedName || ComponentClass.name;
-
-  return patch(ComponentClass, name, console);
+  return wrap(ComponentClass, forcedDisplayedName);
 };
